17b79ed7f4cdf916f6d3d3688802336da3e4597b
Mieczyslaw Nalewaj <namiltd@yahoo.com>
2025-11-16 14:01:53 +0100

    drivers: rtl8365mb: set mc index for rtl8367d family chips
    Set mc index for rtl8367d family chips
    
    Signed-off-by: Mieczyslaw Nalewaj <namiltd@yahoo.com>

--- drivers/net/dsa/realtek/rtl8365mb.c
+++ drivers/net/dsa/realtek/rtl8365mb.c
@@ -383,6 +383,11 @@
 #define RTL8365MB_VLAN_PVID_CTRL_OFFSET(port) \
 	(((port) & 1) << 3)
 
+#define RTL8367D_VLAN_PVID_CTRL_BASE			0x0700
+#define RTL8367D_VLAN_PVID_CTRL_REG(port) \
+	(RTL8367D_VLAN_PVID_CTRL_BASE + (port))
+#define RTL8367D_VLAN_PVID_CTRL_MASK			0xFFF
+
 /* VLAN 4k table entry */
 #define RTL8365MB_VLAN_4K_ENTRY_SIZE			3 /* 48-bits */
 #define RTL8365MB_VLAN_4K_CONF0_MEMBERS_LS_MASK		GENMASK(7, 0)
@@ -1293,7 +1298,7 @@ static int rtl8365mb_vlanmc_set(struct dsa_switch *ds, int port,
 	}
 
 	ret = regmap_read(priv->map,
-			  RTL8365MB_VLAN_PVID_CTRL_REG(port),
+			  mb->family_c ? RTL8365MB_VLAN_PVID_CTRL_REG(port) : RTL8367D_VLAN_PVID_CTRL_REG(port),
 			  &data);
 	if (ret) {
 		if (extack)
@@ -1382,10 +1387,17 @@ static int rtl8365mb_vlanmc_set(struct dsa_switch *ds, int port,
 			dev_dbg(priv->dev, "Set port %d PVID to %d (@ %d idx)\n",
 				port, vlan->vid, vlanmc_idx);
 
-			ret = regmap_update_bits(priv->map,
-				 RTL8365MB_VLAN_PVID_CTRL_REG(port),
-				 RTL8365MB_VLAN_PVID_CTRL_MASK(port),
-				 vlanmc_idx << RTL8365MB_VLAN_PVID_CTRL_OFFSET(port));
+			if (mb->family_c) {
+				ret = regmap_update_bits(priv->map,
+					RTL8365MB_VLAN_PVID_CTRL_REG(port),
+					RTL8365MB_VLAN_PVID_CTRL_MASK(port),
+					vlanmc_idx << RTL8365MB_VLAN_PVID_CTRL_OFFSET(port));
+			} else {
+				ret = regmap_update_bits(priv->map,
+					RTL8367D_VLAN_PVID_CTRL_REG(port),
+					RTL8367D_VLAN_PVID_CTRL_MASK,
+					vlanmc_idx & RTL8367D_VLAN_PVID_CTRL_MASK);
+			}
 			if (ret) {
 				if (extack)
 					NL_SET_ERR_MSG_MOD(extack,
